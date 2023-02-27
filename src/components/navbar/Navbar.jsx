6bbd554f59bc2a35b8db51ad25b5bc159356a778
carson <carson_97@live.ca>
2023-02-26 19:48:26 -0800

    Initial Upload
    Finishing nav bar and mobile functionality

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/CS.png'
+
+const Menu = () => (
+  <>
+    <p>
+      <a href='#home'>Home</a>
+    </p>
+    <p>
+      <a href='#AboutMe'>About</a>
+    </p>
+    <p>
+      <a href='#Resumé'>Resumé</a>
+    </p>
+    <p>
+      <a href='#Contact'>Contact</a>
+    </p>
+  </>
+)
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  return (
+    <div className='website_navbar'>
+      <div className='website_navbar-links'>
+        <div className='website_navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='website_navbar-links_container'>
+          <Menu />
+        </div>
+      </div>
+
+      <div className='website_navbar-menu'>
+        {toggleMenu ? (
+          <RiCloseLine
+            color='#fff'
+            size={50}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color='#fff'
+            size={50}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className='website_navbar-menu_container slide-fwd-center'>
+            <div className='website_navbar-menu_links'>
+              <Menu />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
